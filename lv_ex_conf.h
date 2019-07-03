eabe3e3d5e2226e72808b2fc5bc01d159b6a99f8
kaiakz <dk1063167695@gmail.com>
2019-07-03 20:31:36 +0800

    This is the initial development for the project.

---
+++ lv_ex_conf.h
@@ -0,0 +1,34 @@
+/**
+ * @file lv_ex_conf.h
+ *
+ */
+
+#ifndef LV_EX_CONF_H
+#define LV_EX_CONF_H
+
+
+/*******************
+ * GENERAL SETTING
+ *******************/
+#define LV_EX_PRINTF       1       /*Enable printf-ing data*/
+#define LV_EX_KEYBOARD     1       /*Add PC keyboard support to some examples (`lv_drvers` repository is required)*/
+#define LV_EX_MOUSEWHEEL   1       /*Add 'encoder' (mouse wheel) support to some examples (`lv_drivers` repository is required)*/
+
+/*******************
+ *   TEST USAGE
+ *******************/
+#define USE_LV_TESTS       1
+
+/*******************
+ * TUTORIAL USAGE
+ *******************/
+#define USE_LV_TUTORIALS   1
+
+/*********************
+ * APPLICATION USAGE
+ *********************/
+
+
+
+#endif /*LV_EX_CONF_H*/
+
