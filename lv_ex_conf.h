96d9006bf520ccd9963ac5698a4eb128071df929
kaiakz <dk1063167695@gmail.com>
2019-07-05 18:47:24 +0800

    Update to Lvgl v6.0.

--- lv_ex_conf.h
+++ lv_ex_conf.h
@@ -17,18 +17,37 @@
 /*******************
  *   TEST USAGE
  *******************/
-#define USE_LV_TESTS       1
+#define LV_USE_TESTS       1
 
 /*******************
  * TUTORIAL USAGE
  *******************/
-#define USE_LV_TUTORIALS   1
+#define LV_USE_TUTORIALS   1
 
 /*********************
  * APPLICATION USAGE
  *********************/
 
+/* Test the graphical performance of your MCU
+ * with different settings*/
+#define LV_USE_BENCHMARK   1
 
+/*A demo application with Keyboard, Text area, List and Chart
+ * placed on Tab view */
+#define LV_USE_DEMO        1
+#if LV_USE_DEMO
+#define LV_DEMO_WALLPAPER  1    /*Create a wallpaper too*/
+#define LV_DEMO_SLIDE_SHOW 0    /*Automatically switch between tabs*/
+#endif
+
+/*MCU and memory usage monitoring*/
+#define LV_USE_SYSMON      1
+
+/*A terminal to display received characters*/
+#define LV_USE_TERMINAL    1
+
+/*Touch pad calibration with 4 points*/
+#define LV_USE_TPCAL       1
 
 #endif /*LV_EX_CONF_H*/
 
