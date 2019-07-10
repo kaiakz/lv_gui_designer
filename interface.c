dcead245fd340e12ec9930fd74b38bae9a00b1f5
kaiakz <dk1063167695@gmail.com>
2019-07-10 23:50:25 +0800

    add some widget into toolbox.

--- interface.c
+++ interface.c
@@ -1,52 +1,194 @@
 #include "lvgl/lvgl.h"
+#include <stdio.h>
 
 lv_obj_t * screen;
 lv_obj_t * tft_win;
 lv_obj_t * toolbox_win;
 lv_obj_t * setting_win;
-void tft_win_create(lv_obj_t * parent);
-void setting_win_create(lv_obj_t * parent);
-void toolbox_win_create(lv_obj_t * parent);
+lv_obj_t * btnn;
+void tft_win_init(lv_obj_t * parent);
+void setting_win_init(lv_obj_t * parent);
+void toolbox_win_init(lv_obj_t * parent);
 static void btn_cb(lv_obj_t * btn, lv_event_t ev);
+
 
 void lv_gui_designer()
 {
-    screen = lv_obj_create(NULL, NULL);
-    lv_scr_load(screen);
-    lv_theme_t * th = lv_theme_night_init(100, NULL);
-    lv_theme_set_current(th);
+    lv_coord_t hres = lv_disp_get_hor_res(NULL);
+    lv_coord_t vres = lv_disp_get_ver_res(NULL);
 
-    toolbox_win_create(screen);
-    tft_win_create(screen);    
-    setting_win_create(screen);
+    screen = lv_obj_create(lv_disp_get_scr_act(NULL), NULL);
+    lv_obj_set_size(screen, hres, vres);
+    lv_theme_t * th = lv_theme_templ_init(100, NULL);
+    lv_theme_set_current(th);
+     
+    toolbox_win_init(screen);
+    tft_win_init(screen);
+    setting_win_init(screen);
 }
 
-void tft_win_create(lv_obj_t * parent)
+void tft_win_init(lv_obj_t * parent)
 {
     tft_win = lv_win_create(parent, NULL);
     lv_win_set_title(tft_win, "TFT Simulator");
-    lv_obj_set_size(tft_win, 200, 100);
-    lv_obj_align(tft_win, parent, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_t * btn = lv_btn_create(tft_win, NULL);
-    lv_obj_set_drag(btn, true);
+    lv_obj_set_size(tft_win, 480, 320);
+    lv_obj_align(tft_win, NULL, LV_ALIGN_CENTER, 0, 0);
+    btnn = lv_btn_create(tft_win, NULL);
+    lv_obj_set_drag(btnn, true);
 }
 
-void toolbox_win_create(lv_obj_t * parent)
+void toolbox_win_init(lv_obj_t * parent)
 {
     toolbox_win = lv_win_create(parent, NULL);
     lv_win_set_title(toolbox_win, "ToolBox");
-    lv_obj_set_size(parent, 200, lv_obj_get_height(parent));
-    lv_obj_align(toolbox_win, toolbox_win, LV_ALIGN_IN_LEFT_MID, 0, 0);
+    lv_obj_set_width(toolbox_win, lv_obj_get_width(parent) / 5);
+    lv_obj_align(toolbox_win, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0);
+
+    lv_theme_t * th = lv_theme_get_current();
+
+    static lv_style_t h_style;
+    lv_style_copy(&h_style, &lv_style_transp);
+    h_style.body.padding.inner = LV_DPI / 10;
+    h_style.body.padding.left = LV_DPI / 10;
+    h_style.body.padding.right = LV_DPI / 10;
+    h_style.body.padding.top = LV_DPI / 10;
+    h_style.body.padding.bottom = LV_DPI / 10;
+
+    lv_obj_t * h = lv_cont_create(toolbox_win, NULL);
+    lv_obj_set_style(h, &h_style);
+    lv_obj_set_click(h, false);
+    lv_cont_set_fit(h, LV_FIT_TIGHT);
+    lv_cont_set_layout(h, LV_LAYOUT_COL_M);
+
+    lv_obj_t * btn = lv_btn_create(h, NULL);
+    lv_btn_set_fit(btn, LV_FIT_TIGHT);
+    lv_btn_set_toggle(btn, true);
+    lv_obj_t * btn_label = lv_label_create(btn, NULL);
+    lv_label_set_text(btn_label, "Button");
+
+    btn = lv_btn_create(h, btn);
+    lv_btn_toggle(btn);
+    btn_label = lv_label_create(btn, NULL);
+    lv_label_set_text(btn_label, "Toggled");
+
+    btn = lv_btn_create(h, btn);
+    lv_btn_set_state(btn, LV_BTN_STATE_INA);
+    btn_label = lv_label_create(btn, NULL);
+    lv_label_set_text(btn_label, "Inactive");
+
+    lv_obj_t * label = lv_label_create(h, NULL);
+    lv_label_set_text(label, "Primary");
+    lv_obj_set_style(label, th->style.label.prim);
+
+    label = lv_label_create(h, NULL);
+    lv_label_set_text(label, "Secondary");
+    lv_obj_set_style(label, th->style.label.sec);
+
+    label = lv_label_create(h, NULL);
+    lv_label_set_text(label, "Hint");
+    lv_obj_set_style(label, th->style.label.hint);
+
+    static const char * btnm_str[] = {"1", "2", "3", LV_SYMBOL_OK, LV_SYMBOL_CLOSE, ""};
+    lv_obj_t * btnm = lv_btnm_create(h, NULL);
+    lv_obj_set_size(btnm, lv_disp_get_hor_res(NULL) / 4, 2 * LV_DPI / 3);
+    lv_btnm_set_map(btnm, btnm_str);
+    lv_btnm_set_btn_ctrl_all(btnm, LV_BTNM_CTRL_TGL_ENABLE);
+    lv_btnm_set_one_toggle(btnm, true);
+
+    lv_obj_t * table = lv_table_create(h, NULL);
+    lv_table_set_col_cnt(table, 3);
+    lv_table_set_row_cnt(table, 4);
+    lv_table_set_col_width(table, 0, LV_DPI / 3);
+    lv_table_set_col_width(table, 1, LV_DPI / 2);
+    lv_table_set_col_width(table, 2, LV_DPI / 2);
+    lv_table_set_cell_merge_right(table, 0, 0, true);
+    lv_table_set_cell_merge_right(table, 0, 1, true);
+
+    lv_table_set_cell_value(table, 0, 0, "Table");
+    lv_table_set_cell_align(table, 0, 0, LV_LABEL_ALIGN_CENTER);
+
+    lv_table_set_cell_value(table, 1, 0, "1");
+    lv_table_set_cell_value(table, 1, 1, "13");
+    lv_table_set_cell_align(table, 1, 1, LV_LABEL_ALIGN_RIGHT);
+    lv_table_set_cell_value(table, 1, 2, "ms");
+
+    lv_table_set_cell_value(table, 2, 0, "2");
+    lv_table_set_cell_value(table, 2, 1, "46");
+    lv_table_set_cell_align(table, 2, 1, LV_LABEL_ALIGN_RIGHT);
+    lv_table_set_cell_value(table, 2, 2, "ms");
+
+    lv_table_set_cell_value(table, 3, 0, "3");
+    lv_table_set_cell_value(table, 3, 1, "61");
+    lv_table_set_cell_align(table, 3, 1, LV_LABEL_ALIGN_RIGHT);
+    lv_table_set_cell_value(table, 3, 2, "ms");
+
+    // h = lv_cont_create(parent, h);
+
+    lv_obj_t * sw_h = lv_cont_create(h, NULL);
+    lv_cont_set_style(sw_h, LV_CONT_STYLE_MAIN, &lv_style_transp);
+    lv_cont_set_fit2(sw_h, LV_FIT_NONE, LV_FIT_TIGHT);
+    lv_obj_set_width(sw_h, LV_HOR_RES / 4);
+    lv_cont_set_layout(sw_h, LV_LAYOUT_PRETTY);
+
+    lv_obj_t * sw = lv_sw_create(sw_h, NULL);
+    lv_sw_set_anim_time(sw, 250);
+
+    sw = lv_sw_create(sw_h, sw);
+    lv_sw_on(sw, LV_ANIM_OFF);
 
 
-    lv_obj_t * btn = lv_btn_create(toolbox_win, NULL);
-    lv_btn_set_style(btn, LV_BTN_STYLE_PR, &lv_style_transp_tight);
-    lv_obj_set_event_cb(btn, btn_cb);
+    lv_obj_t * bar = lv_bar_create(h, NULL);
+    lv_bar_set_value(bar, 70, false);
 
+    lv_obj_t * slider = lv_slider_create(h, NULL);
+    lv_bar_set_value(slider, 70, false);
 
+    lv_obj_t * line = lv_line_create(h, NULL);
+    static lv_point_t line_p[2];
+    line_p[0].x = 0;
+    line_p[0].y = 0;
+    line_p[1].x = lv_disp_get_hor_res(NULL) / 5;
+    line_p[1].y = 0;
+
+    lv_line_set_points(line, line_p, 2);
+    lv_line_set_style(line, LV_LINE_STYLE_MAIN, th->style.line.decor);
+
+    lv_obj_t * cb = lv_cb_create(h, NULL);
+
+    cb = lv_cb_create(h, cb);
+    lv_btn_set_state(cb, LV_BTN_STATE_TGL_REL);
+
+    lv_obj_t * ddlist = lv_ddlist_create(h, NULL);
+    lv_ddlist_set_fix_width(ddlist, lv_obj_get_width(ddlist) + LV_DPI / 2);   /*Make space for the arrow*/
+    lv_ddlist_set_draw_arrow(ddlist, true);
+
+    // h = lv_cont_create(parent, h);
+
+    lv_obj_t * list = lv_list_create(h, NULL);
+    lv_obj_set_size(list, lv_disp_get_hor_res(NULL) / 4, lv_disp_get_ver_res(NULL) / 2);
+    lv_obj_t * list_btn;
+    list_btn = lv_list_add_btn(list, LV_SYMBOL_GPS,  "GPS");
+    lv_btn_set_toggle(list_btn, true);
+
+    lv_list_add_btn(list, LV_SYMBOL_WIFI, "WiFi");
+    lv_list_add_btn(list, LV_SYMBOL_GPS, "GPS");
+    lv_list_add_btn(list, LV_SYMBOL_AUDIO, "Audio");
+    lv_list_add_btn(list, LV_SYMBOL_VIDEO, "Video");
+    lv_list_add_btn(list, LV_SYMBOL_CALL, "Call");
+    lv_list_add_btn(list, LV_SYMBOL_BELL, "Bell");
+    lv_list_add_btn(list, LV_SYMBOL_FILE, "File");
+    lv_list_add_btn(list, LV_SYMBOL_EDIT, "Edit");
+    lv_list_add_btn(list, LV_SYMBOL_CUT,  "Cut");
+    lv_list_add_btn(list, LV_SYMBOL_COPY, "Copy");
+
+    lv_obj_t * roller = lv_roller_create(h, NULL);
+    lv_roller_set_options(roller, "Monday\nTuesday\nWednesday\nThursday\nFriday\nSaturday\nSunday", true);
+    lv_roller_set_selected(roller, 1, false);
+    lv_roller_set_visible_row_count(roller, 3);
+   
 }
 
-void setting_win_create(lv_obj_t * parent)
+void setting_win_init(lv_obj_t * parent)
 {
     
 }
@@ -56,5 +198,10 @@ static void btn_cb(lv_obj_t * btn, lv_event_t ev)
     if(ev == LV_EVENT_PRESSED)
     {
         lv_obj_set_size(tft_win, lv_obj_get_width(tft_win) + 2, lv_obj_get_height(tft_win) + 2);
+        lv_area_t ar;
+        lv_obj_get_coords(btnn, &ar);
+        printf("%d %d %d %d", ar.x1, ar.y1, ar.x2, ar.y2);
+        
+
     }
 }
---
+++ interface.h
@@ -0,0 +1,8 @@
+#ifndef _INTERFACE_H_
+#define _INTERFACE_H_
+
+void lv_gui_designer(void);
+
+
+
+#endif
